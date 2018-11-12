b5014fa3a6f4369368bc95a740e6e25e6ebdbd12
michael zavattaro <mwzavattaro@gmail.com>
2018-11-12 18:34:06 +1100

    Working on cars user story, incomplete

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,15 @@
 class CarsController < ApplicationController
+  def index
+    @cars = Car.all
+  end
+
+  # def create
+  # @car = Car.new(car_params)
+  # @booking = Booking.find(params[:booking_id])
+  # @car.booking = @booking
+  # end
+
+  # def show
+  # # @car = Car.find
+  # end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,9 @@
 class Car < ApplicationRecord
   belongs_to :user
+  belongs_to :booking
+  
+  validates :make, presence: true
+  validates :model, presence: true
+  validates :year, presence: true
+  validates :price, presence: true
 end
