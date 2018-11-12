7c126eff2a39ad8c5a58b91c7d5ad0c981ea13e2
michael zavattaro <mwzavattaro@gmail.com>
2018-11-13 10:57:44 +1100

    Cars Controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,20 @@
 class CarsController < ApplicationController
+  def index
+    @cars = Car.all
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @car.user = current_user
+    
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def car_params
+    params.require(:car).permit(:make, :model, :year, :price)
+  end
+  
 end
