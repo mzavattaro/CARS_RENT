7b5ad60c864029d6a4e5b13fd78d99a5d28542bd
michael zavattaro <mwzavattaro@gmail.com>
2018-11-13 16:14:51 +1100

    Completed my bookings view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/mybookings", to: "bookings#my_booking"
+  get "/mybookings", to: "bookings#my_bookings"
   get "/rentyourcar", to: "bookings#rent_your_car"
   patch "/mybookings/:booking_id", to: "bookings#update"
 
