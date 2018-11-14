f81616c00d4340454af1432308e701c4e1ae19e1
NGOC HOA (DAVID) BUI <buingochoa1987@gmail.com>
2018-11-14 12:02:31 +1100

    update navbar and booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,14 +7,14 @@ class BookingsController < ApplicationController
   def create
     @car = Car.find(params[:id])
     if current_user.id == @car.id
-      
+      flash[:notice] = "You can not book your car!!"
+      redirect_to cars_path
     else
       @booking = Booking.new
       @booking.user = current_user
       @booking.car_id = params[:id]
       @booking.save
     redirect_to mybookings_path
-      
     end
     
   end
