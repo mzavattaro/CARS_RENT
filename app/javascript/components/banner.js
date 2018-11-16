7971555718c2543614b01a9e7ebd1c85b5b419b4
michael zavattaro <mwzavattaro@gmail.com>
2018-11-16 14:36:28 +1100

    Show page fronted & Banner fix

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,14 +1,14 @@
 import Typed from 'typed.js';
-const typedElement = document.getElementById('banner-typed-text')
 
-if (typedElement) {
-  function loadDynamicBannerText() {
+function loadDynamicBannerText() {
+  const typedElement = document.getElementById('banner-typed-text')
+  if (typedElement) {
     new Typed('#banner-typed-text', {
       strings: ["This is the place for cars"],
       typeSpeed: 50,
       loop: true
     });
   }
-}
+} 
 
 export { loadDynamicBannerText };
