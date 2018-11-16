dece72f4e56a48c3375c4ba8095db9d61bdb7c2c
michael zavattaro <mwzavattaro@gmail.com>
2018-11-16 13:57:46 +1100

    Changed the banner JS to getElementById

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,14 @@
 import Typed from 'typed.js';
+const typedElement = document.getElementById('banner-typed-text')
 
-function loadDynamicBannerText() {
-  new Typed('#banner-typed-text', {
-    strings: ["This is the place for cars"],
-    typeSpeed: 50,
-    loop: true
-  });
+if (typedElement) {
+  function loadDynamicBannerText() {
+    new Typed('#banner-typed-text', {
+      strings: ["This is the place for cars"],
+      typeSpeed: 50,
+      loop: true
+    });
+  }
 }
 
 export { loadDynamicBannerText };
